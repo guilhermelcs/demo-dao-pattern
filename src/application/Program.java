1841a5e460d0a4de3d331bbdbfe6997a17682e81
Guilherme Lucas <guilherme.evandf@gmail.com>
2020-07-13 16:20:45 -0300

    Delete Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,14 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Department dp = new Department(1, "Computers");
-		Seller seller = new Seller(null, "Gabriella", "gaby@gmail.com", new Date(), 12500.00, dp);
-		sellerDao.insert(seller);
+		sellerDao.deleteById(9);
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -61,21 +61,55 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 		finally {
 			DB.closeStatement(st);
-			DB.closeConnection();
 		}
 			
 	}
 
 	@Override
 	public void update(Seller obj) {
+		PreparedStatement st = null;
 		
-		
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller " +
+					"SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " +
+					"WHERE Id = ?" , Statement.RETURN_GENERATED_KEYS
+					);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new Date( obj.getBirthDate().getTime()) );
+			st.setDouble(4, obj.getSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+		}
+		catch(SQLException e) {
+			throw new DbException( e.getMessage() );
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
+		PreparedStatement st = null;
 		
-		
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM seller " +
+					"WHERE Id = ?"
+					);
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		}
+		catch(SQLException e) {
+			throw new DbException( e.getMessage() );
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
